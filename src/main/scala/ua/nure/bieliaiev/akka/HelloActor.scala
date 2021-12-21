5f668480ecfedefadca0bc921aba0411846a27c2
Bieliaiev, Vladyslav <vladbelyaev55@gmail.com>
2021-12-21 21:11:19 +0200

    Update akka actors

--- src/main/scala/ua/nure/bieliaiev/akka/HelloActor.scala
+++ src/main/scala/ua/nure/bieliaiev/akka/HelloActor.scala
@@ -4,13 +4,12 @@ import akka.actor._
 
 class HelloActor(myName: String) extends Actor {
   def receive: Receive = {
-    // (2) changed these println statements
     case "hello" => println("hello from %s".format(myName))
     case _       => println("'huh?', said %s".format(myName))
   }
 }
 
-@main def helloActor(): Unit =
+@main def helloActorMain(): Unit =
   val system = ActorSystem("HelloSystem")
   val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
   helloActor ! "hello"
--- src/main/scala/ua/nure/bieliaiev/akka/HelloActorNewApi.scala
+++ src/main/scala/ua/nure/bieliaiev/akka/HelloActorNewApi.scala
@@ -1,19 +1,27 @@
 package ua.nure.bieliaiev.akka
 
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, PostStop}
 import akka.actor.typed.scaladsl.Behaviors
 
 object HelloActorNewApi {
+  var isStopped: Boolean = false
   def apply(myName: String): Behavior[String] = Behaviors.receive { (context, message) =>
     message match {
       case "hello" => println("hello from %s".format(myName))
+      case "bye" =>
+        println("bye from %s".format(myName))
+        isStopped = true
       case _ => println("'huh?', said %s".format(myName))
     }
-    Behaviors.same
+    if (isStopped) Behaviors.stopped
+    else Behaviors.same
   }
 }
 
-@main def helloActorNewApi(): Unit =
+@main def helloActorNewApiMain(): Unit =
   val helloActor: ActorSystem[String] = ActorSystem(HelloActorNewApi("Fred"), "helloactor")
   helloActor ! "hello"
   helloActor ! "buenos dias"
+  helloActor ! "bye"
+  Thread.sleep(3000)
+  helloActor ! "hello"
